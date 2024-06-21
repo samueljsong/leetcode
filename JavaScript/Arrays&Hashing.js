357c58a2244dd918dc3fd168aeea88c441e82fd3
samueljsong <semuall0108@gmail.com>
2024-06-21 16:32:56 -0700

    daily

--- JavaScript/Arrays&Hashing.js
+++ JavaScript/Arrays&Hashing.js
@@ -138,3 +138,63 @@ var productExceptSelf = function (nums) {
 
     return result;
 };
+
+/**
+ * Roman to integer
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    const pairs = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
+    let res = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        pairs[s[i]] < pairs[s[i + 1]]
+            ? (res -= pairs[s[i]])
+            : (res += pairs[s[i]]);
+    }
+
+    return res;
+};
+
+const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const rom = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+];
+
+/**
+ * Integer to Roman
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+    let result = "";
+    for (let i = 0; num; i++) {
+        while (num >= val[i]) {
+            result += rom[i];
+            num -= val[i];
+        }
+    }
+    return result;
+};
