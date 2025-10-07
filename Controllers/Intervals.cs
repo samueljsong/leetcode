3efd802a0261db53389a83cc8ccdff9ff2c2c948
sam <semuall0108@gmail.com>
2025-10-06 20:18:31 -0700

    daily leetcode

--- Controllers/Intervals.cs
+++ Controllers/Intervals.cs
@@ -5,6 +5,51 @@
 /// Sorting by Start Time:
 ///     - makes it easier to merge two intervals that are overlapping.
 ///     - overlapping intervals: 
+///     - After sorting by start time, an interval may overlap a previous interval if the start time happens before the previous intervals end time.
+///         - Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+///           for (int i = 0 ; i < intervals.Length; i++)
+///           {
+///               if (intervals[i][0] < intervals[i - 1][1])
+///                   return false
+///           }
+///           return true
+///
+///     - Merging intervals:
+///         - when an interval overlaps, with the previous interval they can be merged into a single interval.
+///         - to merge: set the end time of the previous interval to be the max of either time.
+///             
+///           
+///           Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+///           List<int[]> merged = new List<int[]>();
+///           foreach (var interval in intervals)
+///           {
+///               if (merged.Count == 0 || interval[0] > merged[^1][1])
+///               {
+///                   merged.Add(interval);
+///               }
+///               else
+///               {
+///                   merged[^1][1] = Math.Max(merged[^1][1], interval[1]);         // ^1 : get the last element
+///               }
+///           }
+/// 
+///           return merged
+/// 
+/// Sorting by End Time:
+///     - we risk adding an interval that starts early but ends late to the set of non overlapping intervals.
+/// 
+///         Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+///         int end = intervals[0][1];
+///         int count = 1;                                              // number of non overlapping intervals.
+///         for (int i = 0; i < intervals.Length; i++)
+///         {
+///             if (intervals[i][0] >= end)
+///             {
+///                 end = intervals[i][1];
+///                 count++;
+///             }
+///         }
+///         return intervals.Length - count;
 /// 
 /// </summary>
 public class Intervals()
